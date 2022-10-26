fc933cd04f768bd2e0903c3142dedded2cbe8655
Juliana Guonik <juliana_guonik@hotmail.com>
2022-10-26 15:02:22 -0300

    Post author using DTO

--- src/main/java/com/jg/workshopmongo/config/Instantiation.java
+++ src/main/java/com/jg/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.jg.workshopmongo.domain.Post;
 import com.jg.workshopmongo.domain.User;
+import com.jg.workshopmongo.dto.AuthorDTO;
 import com.jg.workshopmongo.repository.PostRepository;
 import com.jg.workshopmongo.repository.UserRepository;
 
@@ -36,10 +37,12 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("25/10/2022"), "Partiu viagem", "Viajando para os EUA!", maria);
-		Post post2 = new Post(null, sdf.parse("26/10/2022"), "Bom dia", "Acordei feliz", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("25/10/2022"), "Partiu viagem", "Viajando para os EUA!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("26/10/2022"), "Bom dia", "Acordei feliz", new AuthorDTO(maria));
+		
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 		
