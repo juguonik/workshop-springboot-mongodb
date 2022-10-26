44e24a149100001b8828361551e762669412f745
Juliana Guonik <juliana_guonik@hotmail.com>
2022-10-26 15:20:15 -0300

    Getting posts by user id

--- src/main/java/com/jg/workshopmongo/resources/UserResource.java
+++ src/main/java/com/jg/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.jg.workshopmongo.domain.Post;
 import com.jg.workshopmongo.domain.User;
 import com.jg.workshopmongo.dto.UserDTO;
 import com.jg.workshopmongo.services.UserService;
@@ -65,6 +66,13 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+		
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 		
 	}
 
