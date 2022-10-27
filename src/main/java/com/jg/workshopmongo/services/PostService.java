5528de2ab89547cc34a5930eb521b5aefc102af1
Juliana Guonik <juliana_guonik@hotmail.com>
2022-10-27 09:36:16 -0300

    Query methods to get posts

--- src/main/java/com/jg/workshopmongo/services/PostService.java
+++ src/main/java/com/jg/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.jg.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,9 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 	 
 
 }
